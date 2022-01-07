c5f0077aefebe480fa68b9667fbf79aac2af0634
Malateshwaddatti <87762245+Malateshwaddatti@users.noreply.github.com>
2022-01-07 22:46:58 +0530

    Add files via upload

---
+++ zendb38.js
@@ -0,0 +1,103 @@
+//find all the topics and tasks which are thought in the month of October
+[
+    {
+      "codecata": "codecata 1",
+      "Users_id": "Users 1",
+      "students_name": "yashwantha",
+      "Tasks": "DSA",
+      "Topics": "Grahaps",
+      "Attendtends": " 1",
+      "Menters": "Jayanth",
+      "date": "10-10-2021"
+    },
+    {
+      "codecata": "codecata 2",
+      "Users_id": "2",
+      "students_name": "Naveen S",
+      "Tasks": "Youtube Clone Task",
+      "Topics": "Html css javascript",
+      "Attendtends": "5",
+      "Menters": " Mohan",
+      "date": "15/10/2021"
+    },
+    {
+      "codecata": "codecata 3",
+      "Users_id": "3",
+      "students_name": "Malates",
+      "Tasks": "Zenclass db",
+      "Topics": "Topics 3",
+      "Attendtends": "10",
+      "Menters": "Mohan",
+      "date": "12/11/2021"
+    },
+    {
+      "codecata": "codecata 4",
+      "Users_id": " 4",
+      "students_name": "vinayak",
+      "Tasks": "Cards",
+      "Topics": "Boostrap4",
+      "Attendtends": "Attendtends 11",
+      "Menters": "Ragav",
+      "date": "21/12/2021"
+    },
+    {
+      "codecata": "codecata 5",
+      "Users_id": "5",
+      "students_name": "Preeti",
+      "Tasks": "Loggin Form",
+      "Topics": "Topics 5",
+      "Attendtends": " 15",
+      "Menters": "Aanda",
+      "date": "12/10/2021"
+    },
+    {
+      "codecata": "codecata 6",
+      "Users_id": "6",
+      "students_name": "karana",
+      "Tasks": "Food Delivery app",
+      "Topics": "html css javascript",
+      "Attendtends": "6",
+      "Menters": "Ragav",
+      "date": "22/12/2021"
+    },
+    {
+      "codecata": "codecata 7",
+      "Users_id/": "7",
+      "students_name": "Anuja",
+      "Tasks": "database model",
+      "Topics": "mysql mongodb ",
+      "Attendtends": "7",
+      "Menters": "Shivukumar",
+      "date": "12/12/2021"
+    },
+    {
+      "codecata": "codecata 8",
+      "Users_id": "8",
+      "students_name": "Shivaraj",
+      "Tasks": "Tictlak game",
+      "Topics": "react js ",
+      "Attendtends": " 8",
+      "Menters": "Ragav",
+      "date": "10/10/2021"
+    },
+    {
+      "codecata": "codecata 9",
+      "Users_id": "9",
+      "students_name": "Varsha",
+      "Tasks": "rest api task",
+      "Topics": "node js experess",
+      "Attendtends": "9",
+      "Menters": "Annada",
+      "date": "10/11/2021"
+    },
+    {
+      "codecata": "codecata 10",
+      "Users_id": "10",
+      "students_name": "Prabha",
+      "Tasks": "Zoom App",
+      "Topics": "react js",
+      "Attendtends": " 10",
+      "Menters": "Rajav",
+      "date": "12/12/2021"
+    }
+  ]
